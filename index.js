4bda83cd03dda237a1f32ab107e05d1a223cdb0f
Jordan Shaw <jordan@halfhelix.com>
2022-01-14 15:37:58 -0500

    removed command parsing

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ const isUrl = (s) => /^https?:\/\//.test(s)
 ) => {
   console.log('%s command "%s"', label, fullCommand)
 
-  const args = cliParser.parse(fullCommand)
-  debug(`parsed command: ${args.join(' ')}`)
+  // const args = cliParser.parse(fullCommand)
+  const args = fullCommand)
+  // debug(`parsed command: ${args.join(' ')}`)
+  debug(`parsed command: ${args}`)
 
   return io.which(args[0], true).then((toolPath) => {
     debug(`found command "${toolPath}"`)
